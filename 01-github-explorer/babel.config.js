e8bd83ef0738ca6967a49a55b4d88c7e53d9a3ac
diego-vini <analistamm@gmail.com>
2021-04-21 23:17:46 -0300

    Iniciando projeto github explorer

---
+++ 01-github-explorer/babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: [
+    //Converte nosso código eno melor formato possivel
+    '@babel/preset-env',
+    //Converte o código html dentro do javascript
+    '@babel/preset-react',
+  ]
+}
